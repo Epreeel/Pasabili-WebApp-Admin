3aa4ef8e4971165f7c94dc74ec590e0147456e1e
Cloyd-Anis <cloyd.anis@gmail.com>
2023-02-18 20:16:45 +0800

    BE - Adjust spacing

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,31 +3,31 @@ const admin = require("../models/admin");
 const express = require('express');
 const jwt = require('jsonwebtoken');
 var C = require("crypto-js");
-const { Op} = require('sequelize');
+const { Op } = require('sequelize');
 var moment = require('moment');
-const {generateAdminAccessToken} = require('../helpers/generateAdminAccessToken');
+const { generateAdminAccessToken } = require('../helpers/generateAdminAccessToken');
 
-exports.login = async(req, res) => {
+exports.login = async (req, res) => {
     let data = await admin.model.findOne({
-        where:{
-            email:req.body.email,
-            status:true
+        where: {
+            email: req.body.email,
+            status: true
         }
     })
-    if(data !== null ){
-        var bytes  = C.AES.decrypt(data.password, process.env.SECRET_KEY);
+    if (data !== null) {
+        var bytes = C.AES.decrypt(data.password, process.env.SECRET_KEY);
         var originalText = bytes.toString(C.enc.Utf8);
-        if(originalText === req.body.password && data.password != ""){
+        if (originalText === req.body.password && data.password != "") {
             const accessToken = generateAdminAccessToken(data);
-            res.send({success:true,message:"Login Successful!",data:data, accessToken})
-        }else{
-            res.send({success:false,message:"The credentials provided does not match."});
+            res.send({ success: true, message: "Login Successful!", data: data, accessToken })
+        } else {
+            res.send({ success: false, message: "The credentials provided does not match." });
         }
-    }else{
-        res.send({success:false,message:"Account not found."});
+    } else {
+        res.send({ success: false, message: "Account not found." });
     }
 }
 
-exports.logout = async(req, res)=>{
+exports.logout = async (req, res) => {
     res.status(200).json("You logged out Successfully");
 }
